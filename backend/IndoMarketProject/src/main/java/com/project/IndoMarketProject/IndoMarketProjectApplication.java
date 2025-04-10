eaa8d2b07cdf06cb78ed0729ee9876aa0da56658
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-04-10 12:19:02 +0100

    feature: set up project

---
+++ backend/IndoMarketProject/src/main/java/com/project/IndoMarketProject/IndoMarketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.IndoMarketProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoMarketProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndoMarketProjectApplication.class, args);
+	}
+
+}
---
+++ backend/IndoMarketProject/src/test/java/com/project/IndoMarketProject/IndoMarketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.IndoMarketProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoMarketProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
